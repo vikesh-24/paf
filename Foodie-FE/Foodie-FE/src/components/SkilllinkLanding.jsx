de02ff8033d3505f770a09aa23c956779553271a
Senthee1811 <esentheepan@gmail.com>
2025-04-26 09:43:23 +0530

    Finished Plan

--- Foodie-FE/Foodie-FE/src/components/SkilllinkLanding.jsx
+++ Foodie-FE/Foodie-FE/src/components/SkilllinkLanding.jsx
@@ -5,7 +5,13 @@ import { Link } from 'react-router-dom';
 export default function CookingSkillsLanding() {
   const [animationProgress, setAnimationProgress] = useState(0);
   
-  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAnimationProgress(100);
+    }, 500);
+    
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div style={{
