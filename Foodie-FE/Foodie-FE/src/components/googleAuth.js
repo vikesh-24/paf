43f112265eb763804e71b7ef87622695e36a61a5
Brinthapan <brinthapanj@gmail.com>
2025-05-10 09:49:01 +0530

    finished Auth

--- Foodie-FE/Foodie-FE/src/components/googleAuth.js
+++ Foodie-FE/Foodie-FE/src/components/googleAuth.js
@@ -21,6 +21,14 @@ function GoogleAuth({ handleAuth }) {
     })
 
     return (
+         <div>
+            <div onClick={handleClick} className="loginButton google" >
+                <img src={google} alt="" className="icon"  />
+                Continue with Google
+            </div>
+
+            {/* <button onClick={handleClick}>Signin With Google</button> */}
+        </div>
        
     );
 }
